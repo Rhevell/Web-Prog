bd4efd209a39fd7713016cbc5af6a4757150995a
Rhevell <rhevellhs@gmail.com>
2025-11-13 22:35:10 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\WritersController;
+// use App\Http\Controllers\PopularController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/about',function(){
+    return view('Main.AboutUs');
+})->name('about');
+Route::get('/categories', [CategoriesController::class, 'index'])->name('categories.index');
+Route::get('/categories/{id}', [CategoriesController::class, 'show'])->name('categories.show');
+Route::get('/articles/{id}', [ArticlesController::class, 'show'])->name('articles.show');
+Route::get('/writers', [WritersController::class, 'index'])->name('writers.index');
+Route::get('/writers/{id}', [WritersController::class, 'show'])->name('writers.show');
+
+
+// Route::get('/popular', [PopularController::class, 'index'])->name('popular');
